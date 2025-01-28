e7048f802721dd65cb0b32b6b3308b6718a27121
Shikhar Pandey <devilshikhar123@gmail.com>
2025-01-29 04:06:26 +0530

    1st code

---
+++ convert2binary.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+string convert2binary(int n){
+    string res="";
+    while(n>0){
+        if (n%2==1)res+="1";
+        else res+="0";   
+        n=n/2;
+    }
+    reverse(res.begin(),res.end());
+    return res;
+}
+int main(){
+    string answer= convert2binary(13);
+    cout<<answer;
+    return 0;
+}
